e09e06ea5ec1bcee1b2f4280563bc99f2ca82989
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-09-20 01:09:37 +0530

    Add files via upload

---
+++ FindDuplicateFileInSystem.cpp
@@ -0,0 +1,63 @@
+/*
+Given a list paths of directory info, including the directory path, and all the files with contents in this directory,
+return all the duplicate files in the file system in terms of their paths. You may return the answer in any order.
+A group of duplicate files consists of at least two files that have the same content.
+A single directory info string in the input list has the following format:
+"root/d1/d2/.../dm f1.txt(f1_content) f2.txt(f2_content) ... fn.txt(fn_content)"
+It means there are n files (f1.txt, f2.txt ... fn.txt) with content (f1_content, f2_content ... fn_content)
+respectively in the directory "root/d1/d2/.../dm". Note that n >= 1 and m >= 0.  If m = 0, it means the directory is just the root directory.
+The output is a list of groups of duplicate file paths. For each group, it contains all the file paths of the files that have the same content.
+A file path is a string that has the following format:
+"directory_path/file_name.txt"
+
+Example 1:
+Input: paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]
+Output: [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+
+Example 2:
+Input: paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)"]
+Output: [["root/a/2.txt","root/c/d/4.txt"],["root/a/1.txt","root/c/3.txt"]]
+*/
+
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> root;
+        for(int i=0;i<paths.size();i++){
+            string base="";
+            int j=0, m=paths[i].length();
+            while(paths[i][j]!=' '){
+                base+=paths[i][j];
+                j++;
+            }
+        j++;
+
+        while(j<m){
+            string header="" , key="";
+            while(paths[i][j]!='('){
+                header+=paths[i][j];
+                j++;
+            }
+            j++;
+            while(paths[i][j]!=')'){
+                key+=paths[i][j];
+                j++;
+            }
+            j+=2;
+            header=base+"/"+header;
+            root[key].push_back(header);
+            }
+        }
+    vector<vector<string>> result;
+    for(auto k : root){
+        vector<string> temp;
+        if(k.second.size()>1){
+            for(auto m : k.second)
+                temp.push_back(m);
+
+            result.push_back(temp);
+        }
+    }
+    return result;
+    }
+};
