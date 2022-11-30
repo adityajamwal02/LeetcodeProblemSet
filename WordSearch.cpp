2782c03a5841292d172f804335b4c56c5a659393
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-11-30 21:43:03 +0530

    Update WordSearch.cpp

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -16,3 +16,36 @@ Output: false
 */
 
 
+class Solution {
+public:
+    bool dfs(int i, int j, int count, vector<vector<char>> &board, string word){
+        if(word.size()==count){
+            return true;
+        }
+        if(i<0 or j<0 or i>=board.size() or j>=board[0].size() or board[i][j]!=word[count]){
+            return false;
+        }
+        char temp=board[i][j];
+        board[i][j]=' ';
+        bool ans = dfs(i-1,j,count+1,board,word) or 
+                   dfs(i+1,j,count+1,board,word) or
+                   dfs(i,j-1,count+1,board,word) or 
+                   dfs(i,j+1,count+1,board,word);
+
+        board[i][j]=temp;
+        return ans;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0] and dfs(i,j,0,board,word)){
+                    return true;
+                }
+            }
+        }
+    return false;
+    }
+};
+
