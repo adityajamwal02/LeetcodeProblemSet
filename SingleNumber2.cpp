67af6507ff642f893dbaef9797a7b69d208d2fdc
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-12-11 02:51:56 +0530

    Add files via upload

---
+++ SingleNumber2.cpp
@@ -0,0 +1,30 @@
+/*
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,3,2]
+Output: 3
+
+Example 2:
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto num : nums){
+            mp[num]++;
+        }
+        int ans;
+        for(auto it : mp){
+            if(it.second==1){
+                ans=it.first;
+            }
+        }
+    return ans;
+    }
+};
