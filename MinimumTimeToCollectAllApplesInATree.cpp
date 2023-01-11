88af2cb89f3d0dd9aca762f06f39a2f79582569c
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-11 12:07:34 +0530

    Add files via upload

---
+++ MinimumTimeToCollectAllApplesInATree.cpp
@@ -0,0 +1,81 @@
+/*
+Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices. You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend to collect all apples in the tree, starting at vertex 0 and coming back to this vertex.
+The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that vertex i has an apple; otherwise, it does not have any apple.
+
+Example 1:
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+Output: 8
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.
+
+Example 2:
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,false,true,false]
+Output: 6
+Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.
+
+Example 3:
+Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,false,false,false,false,false]
+Output: 0
+*/
+
+class Solution {
+public:
+    int dfs(int prev, int curr, vector<vector<int>> &adj, vector<bool> &hasApple){
+        int ans=0;
+        for(auto x: adj[curr]){
+            if(x!=prev){
+                int res=dfs(curr, x, adj, hasApple);
+                if(res>0 or hasApple[x]){
+                    ans+=(res+2);
+                }
+            }
+        }
+    return ans;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>> adj(n);
+        for(auto x: edges){
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+    return dfs(-1, 0, adj, hasApple);
+    }
+};
+
+/// Another DFS
+
+class Solution {
+public:
+    int time=0;
+    bool helper(vector<vector<int>>&graph,vector<bool>&apple,int node,int parent)
+    {
+        bool ok=false;
+        if(apple[node])
+        {
+            ok=true;
+        }
+        for(auto child:graph[node])
+        {
+            if(child!=parent)
+            {
+                if(helper(graph,apple,child,node))
+                {
+                    ok=true;
+                    time+=2;
+                }
+            }
+        }
+
+        return ok;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>>graph(n);
+        for(int i=0;i<n-1;i++)
+        {
+            int u=edges[i][0],v=edges[i][1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        helper(graph,hasApple,0,-1);
+        return time;
+    }
+};
