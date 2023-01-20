320b79c99d1048127586aeaa3b1c0e52a9e7640d
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-21 00:49:13 +0530

    Add files via upload

---
+++ IsomorphicStrings.cpp
@@ -0,0 +1,38 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+*/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<int, int> mp1, mp2;
+        for(int i=0;i<s.size();i++){
+            if(mp1.find(s[i])==mp1.end() and mp2.find(t[i])==mp2.end()){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }else if((mp1.find(s[i])==mp1.end() and mp2.find(t[i])!=mp2.end()) or (mp1.find(s[i])!=mp1.end() and mp2.find(t[i])==mp2.end())){
+                return false;
+            }
+            else{
+                if(mp1[s[i]]!=t[i] or mp2[t[i]]!=s[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
