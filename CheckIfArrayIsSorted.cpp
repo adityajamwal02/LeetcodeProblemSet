69800fc7616449d8618989108d431a28d7a7669e
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-02-26 02:32:32 +0530

    Add files via upload

---
+++ CheckIfArrayIsSorted.cpp
@@ -0,0 +1,53 @@
+/*
+Given an array arr[] of size N, check if it is sorted in non-decreasing order or not.
+
+Example 1:
+Input:
+N = 5
+arr[] = {10, 20, 30, 40, 50}
+Output: 1
+Explanation: The given array is sorted.
+
+Example 2:
+Input:
+N = 6
+arr[] = {90, 80, 100, 70, 40, 30}
+Output: 0
+Explanation: The given array is not sorted.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function arraySortedOrNot() which takes the arr[] and N as input parameters and returns a boolean value (true if it is sorted otherwise false).
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+*/
+
+
+
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        if(n==1){
+            return true;
+        }
+        else if(n==2){
+            if(arr[0]<arr[1]){
+                return true;
+            }
+        return false;
+        }
+        else {
+            for(int i=1;i<n-1;i++){
+                if(arr[i]>=arr[i-1] and arr[i]<=arr[i+1]){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+    return true;
+    }
+};
