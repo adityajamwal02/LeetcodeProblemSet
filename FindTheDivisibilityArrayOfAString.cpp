8c720616990962d4afff376e68a99484302eab4e
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-02-26 21:04:46 +0530

    Add files via upload

---
+++ FindTheDivisibilityArrayOfAString.cpp
@@ -0,0 +1,31 @@
+/*
+You are given a 0-indexed string word of length n consisting of digits, and a positive integer m.
+The divisibility array div of word is an integer array of length n such that:
+div[i] = 1 if the numeric value of word[0,...,i] is divisible by m, or
+div[i] = 0 otherwise.
+Return the divisibility array of word.
+
+Example 1:
+Input: word = "998244353", m = 3
+Output: [1,1,0,0,0,1,1,0,0]
+Explanation: There are only 4 prefixes that are divisible by 3: "9", "99", "998244", and "9982443".
+
+Example 2:
+Input: word = "1010", m = 10
+Output: [0,1,0,1]
+Explanation: There are only 2 prefixes that are divisible by 10: "10", and "1010".
+*/
+
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, long long int m) {
+        int n=word.size();
+        vector<int> result(n);
+        long long temp=0;
+        for(int i=0;i<n;i++){
+            temp=(10*temp+(word[i]-'0'))%m;
+            result[i]=(temp==0);
+        }
+    return result;
+    }
+};
