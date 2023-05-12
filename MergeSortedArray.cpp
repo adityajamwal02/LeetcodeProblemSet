a086d4dd2e59bde3cc93d6070dac780d43fd23a4
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-05-13 00:13:19 +0530

    Update MergeSortedArray.cpp

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -20,3 +20,21 @@ public:
             nums1[i]=v[i];
     }
 };
+
+/// Optimized 2 pointer solution
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1, j=n-1, k=m+n-1;
+        while(i>=0 and j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }else{
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(i>=0)  nums1[k--]=nums1[i--];
+        while(j>=0)  nums1[k--]=nums2[j--];
+    }
+};
