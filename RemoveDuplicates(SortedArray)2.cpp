fcab31656ca9c374bf83e91b4d393cbe2d30c4bc
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-05-13 23:51:24 +0530

    Update RemoveDuplicates(SortedArray)2.cpp

--- RemoveDuplicates(SortedArray)2.cpp
+++ RemoveDuplicates(SortedArray)2.cpp
@@ -61,3 +61,31 @@ public:
         return i+2;
     }
 };
+
+// Another Approach [ 2 pointers ]
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 1, j = 1;
+        int count = 1;
+
+        while (i<n) {
+            if (nums[i] == nums[i-1]) {
+                if (count < 2) {
+                    count++; 
+                    nums[j++] = nums[i];
+                }
+            }
+            else {
+                count = 1;
+                nums[j++] = nums[i];
+            }
+
+            i++;
+        }
+
+        return j;
+    }
+};
