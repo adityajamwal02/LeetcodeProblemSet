83af64e01fe409297d59f949778994537e71dda9
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-05-14 00:04:46 +0530

    Update RotateArray.cpp

--- RotateArray.cpp
+++ RotateArray.cpp
@@ -10,6 +10,8 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 */
 
+/// Extra  O(n) space
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -20,3 +22,14 @@ public:
     nums=temp;
     }
 };
+
+
+/// Optimized Code with in-place
+
+k=k%nums.size();
+reverse(nums.begin(),nums.begin()+(nums.size()-k));
+reverse(nums.begin()+(nums.size()-k),nums.end());
+reverse(nums.begin(),nums.end());
+            
+
+        
